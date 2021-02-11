40bcaa5b69034e2f06d5450560e3e7c623e7b85d
Michael-dev-coder <michaelnyoni76@gmail.com>
2021-02-11 19:18:42 +0200

    My first Angular App

---
+++ app.js
@@ -0,0 +1,22 @@
+(function () {
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$injector = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.dishes = "";
+    $scope.message = "";
+
+    $scope.checkLunch = function(){
+      var words = $scope.dishes.split(",")
+      if ($scope.dishes == "") {
+        return $scope.message="Please enter data first";
+      }else if (words >= words[3]) {
+        return $scope.message="Too much";
+      }else {
+        return $scope.message="Enjoy!";
+      };
+    };
+
+  };
+})();
