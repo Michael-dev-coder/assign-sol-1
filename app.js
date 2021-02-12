c04ba7c372bcdcfb59924c3af47dfd8495e5f593
Michael-dev-coder <michaelnyoni76@gmail.com>
2021-02-12 12:10:05 +0200

    added the ng-style directive

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  
+
   angular.module('LunchCheck', [])
   .controller('LunchCheckController', LunchCheckController);
   LunchCheckController.$injector = ['$scope'];
@@ -8,14 +8,22 @@
   function LunchCheckController($scope){
     $scope.dishes = "";
     $scope.message = "";
+    $scope.Obj = {};
+    $scope.Obj2 = {};
 
     $scope.checkLunch = function(){
       var words = $scope.dishes.split(",")
       if ($scope.dishes == "") {
+        $scope.Obj = {"color":"red"};
+        $scope.Obj2 = {"border": "1px solid red"};
         return $scope.message="Please enter data first";
       }else if (words >= words[3]) {
+        $scope.Obj = {"color":"green"};
+        $scope.Obj2 = {"border": "1px solid green"};
         return $scope.message="Too much";
       }else {
+          $scope.Obj = {"color":"green"};
+          $scope.Obj2 = {"border": "1px solid green"};
         return $scope.message="Enjoy!";
       };
     };
