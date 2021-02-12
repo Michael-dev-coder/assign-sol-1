38b1c0b1cf382fb27587aff967136672bff67d16
Michael-dev-coder <michaelnyoni76@gmail.com>
2021-02-12 16:13:51 +0200

    used the length function in the conditional statement instead of words >= words[3].

--- app.js
+++ app.js
@@ -17,10 +17,10 @@
         $scope.Obj = {"color":"red"};
         $scope.Obj2 = {"border": "1px solid red"};
         return $scope.message="Please enter data first";
-      }else if (words >= words[3]) {
+      }else if (words.length >= 3) {
         $scope.Obj = {"color":"green"};
         $scope.Obj2 = {"border": "1px solid green"};
-        return $scope.message="Too much";
+        return $scope.message="Too much!";
       }else {
           $scope.Obj = {"color":"green"};
           $scope.Obj2 = {"border": "1px solid green"};
